4813ae43c80b1465c9a22090449fc70e34da7ca5
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-01-05 21:46:28 +0100

    makes authors optional :sunglasses: & corrects what I had broken by adding a column :see_no_evil:

--- server.R
+++ server.R
@@ -6,6 +6,7 @@ packages <- dplyr::distinct(
   view, package,
   .keep_all = TRUE
 )
+
 packages$license <- sub("( [+|] )?file LICEN[SC]E", "", packages$license, ignore.case = FALSE)
 
 decorate_url <- function(url) {
@@ -25,8 +26,9 @@ shinyServer(function(input, output) {
     if (input$task) {
       drop_columns <- NULL
       if (length(input$view) == 1) drop_columns <- 1
-      if (!input$description) drop_columns <- c(drop_columns, 5)
-      if (!input$url) drop_columns <- c(drop_columns, 6)
+      if (!input$description) drop_columns <- c(drop_columns, 6)
+      if (!input$url) drop_columns <- c(drop_columns, 7)
+      if (!input$authors) drop_columns <- c(drop_columns, 4)
       if (length(input$view) > 1) {
         temporary <- packages[packages$view %in% input$view, ]
         if (any(duplicated(temporary$package))) {
@@ -52,8 +54,9 @@ shinyServer(function(input, output) {
       }
     } else {
       drop_columns <- NULL
-      if (!input$description) drop_columns <- c(drop_columns, 6)
-      if (!input$url) drop_columns <- c(drop_columns, 7)
+      
+      if (!input$description) drop_columns <- c(drop_columns, 7)
+      if (!input$url) drop_columns <- c(drop_columns, 8)
       temporary <- packages %>%
         dplyr::group_by(package, title, license, description, url, authors) %>%
         dplyr::summarize(view = paste0(view, collapse = ", "),
